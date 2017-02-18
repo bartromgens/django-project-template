2957820ea94c5d69dc053d5909475da8f2880686
Bart Römgens <bart.romgens@gmail.com>
2017-02-18 19:04:16 +0100

    create example app

--- website/settings.py
+++ website/settings.py
@@ -11,6 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 INSTALLED_APPS = [
     'website',
+    'exampleapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
