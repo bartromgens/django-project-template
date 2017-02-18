a6dae26cf43a12aa604de95e84ee6e56da366082
Bart Römgens <bart.romgens@gmail.com>
2017-02-18 18:33:03 +0100

    create initial Django project template

---
+++ website/local_settings_example.py
@@ -0,0 +1,35 @@
+# user settings, included in settings.py
+
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+# SECURITY WARNING: Make this unique, and don't share it with anybody.
+SECRET_KEY = ''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(BASE_DIR, 'dev.sqlite'),                      # Or path to database file if using sqlite3.
+    }
+}
+
+LANGUAGE_CODE = 'en-US'
+
+TIME_ZONE = "Europe/Amsterdam"
+
+#STATIC_ROOT = '/home/username/webapps/<staticdir>/'
+STATIC_ROOT = ''
+
+#STATIC_URL = '//www.domain.com/static/'
+STATIC_URL = '/static/'
+
+#MEDIA_ROOT = '/home/<username>/webapps/<projectstatic>/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'website/static/media/')
+
+#MEDIA_URL = '//www.<your-domain>.com/static/media/'
+MEDIA_URL = '/static/media/'
---
+++ website/settings.py
@@ -0,0 +1,82 @@
+"""
+Django settings for website project.
+"""
+
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Application definition
+
+INSTALLED_APPS = [
+    'website',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'website.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.template.context_processors.static',
+                'django.template.context_processors.media',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'website.wsgi.application'
+
+# Password validation
+# https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.10/topics/i18n/
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+INTERNAL_IPS = ['127.0.0.1']  # needed for django-debug-toolbar
