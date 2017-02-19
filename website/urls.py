c6ffad1d64129de2beeeb622ff50883c52002aae
Bart Römgens <bart.romgens@gmail.com>
2017-02-19 15:04:11 +0100

    create userprofile update page

--- website/urls.py
+++ website/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
+
+from website.views import UserProfileView
 
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='website/index.html'), name='homepage'),
+    url(r'^userprofile/(?P<pk>[0-9]+)/$', login_required(UserProfileView.as_view())),
 
     url(r'^accounts/', include('registration.backends.simple.urls')),  # the django-registration module
 
---
+++ website/views.py
@@ -0,0 +1,24 @@
+from django.core.exceptions import PermissionDenied
+from django.contrib.auth.models import User
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views.generic import TemplateView, UpdateView
+
+
+class UserProfileView(SuccessMessageMixin, UpdateView):
+    model = User
+    template_name = 'website/userprofile.html'
+    fields = ['username', 'first_name', 'last_name', 'email']
+    success_message = 'Userprofile saved'
+
+    def form_valid(self, form):
+        if self.request.user != form.instance:
+            raise PermissionDenied
+        email = form.cleaned_data['email']
+        users = User.objects.exclude(id=self.request.user.id).filter(email=email)
+        if users.exists():
+            form.add_error('email', 'Email is already used by another user.')
+            return self.form_invalid(form)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return '/userprofile/' + str(self.request.user.id) + '/'
